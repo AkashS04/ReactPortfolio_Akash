200a483a91c26fc22790695068a44a96f5c34428
AkashS04 <akashs10111212@gmail.com>
2024-10-20 10:36:07 +0530

    transition efffect update in projects

--- src/project_details/Project.tsx
+++ src/project_details/Project.tsx
@@ -7,11 +7,31 @@ import PortfolioImg from "../assets/MyProjects/PortfolioWebsite.jpg";
 
 const Project = () => {
   const projectData: any = [
-    { img: EcommerceImg, title: "ECommerse Websites",content:"Robust ECommerce Websites for Clients" },
-    { img: BussinessImg,title: "Bussiness Websites",content:"Robust Bussiness Websites for Clients" },
-    { img: StartupImg,title: "StartUp Websites",content:"Robust StartUp Websites for Clients" },
-    { img: PersonalImg,title: "Personal Websites",content:"Robust Personal Websites for Clients" },
-    { img: PortfolioImg,title: "Portfolio Websites",content:"Robust Portfolio Websites for Clients" },
+    {
+      img: EcommerceImg,
+      title: "ECommerse Websites",
+      content: "Robust ECommerce Websites for Clients",
+    },
+    {
+      img: BussinessImg,
+      title: "Bussiness Websites",
+      content: "Robust Bussiness Websites for Clients",
+    },
+    {
+      img: StartupImg,
+      title: "StartUp Websites",
+      content: "Robust StartUp Websites for Clients",
+    },
+    {
+      img: PersonalImg,
+      title: "Personal Websites",
+      content: "Robust Personal Websites for Clients",
+    },
+    {
+      img: PortfolioImg,
+      title: "Portfolio Websites",
+      content: "Robust Portfolio Websites for Clients",
+    },
   ];
 
   return (
@@ -24,23 +44,63 @@ const Project = () => {
         >
           PROJECTS
         </Typography>
-
-        <Box sx={{}}>
-        {projectData.map((data:any)=>
-            (<Box>
-                <Box
-                  component="img"
-                  sx={{ height: "auto", width: "300px" }}
-                  src={data.img}
-                />
-                <Typography variant="h5" gutterBottom>
-                  {data.title}
-                </Typography>
-                <Typography variant="body1" gutterBottom>
-                  {data.content}
-                </Typography>
-              </Box>)
-        )}
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            padding: "2rem 4rem 2rem 4rem",
+            justifyContent: "space-around",
+          }}
+        >
+          {projectData.map((data: any) => (
+            <Box
+              sx={{
+                width: "300px",
+                backgroundColor: "#181818",
+                position: "relative",
+                overflow: "hidden",
+                padding: "2rem",
+                border: "1px solid #121212",
+                borderRadius: "1rem",
+                marginBottom: "2rem",
+                cursor: "default",
+                "::after": {
+                  content: '""',
+                  position: "absolute",
+                  height: "400px",
+                  width: "400px",
+                  top: "0px",
+                  left: "0px",
+                  borderRadius: "1rem",
+                  backgroundColor: "rgb(0,0,0,0.5)",
+                  transition: "transform 0.3s ease",
+                },
+                "&:hover::after": {
+                  transform: "translate(370px, 500px)",
+                },
+              }}
+            >
+              <Box
+                component="img"
+                sx={{ height: "auto", width: "300px" }}
+                src={data.img}
+              />
+              <Typography
+                variant="h5"
+                sx={{ fontFamily: "Raleway-Extralight", color: "#e3e3e3" }}
+                gutterBottom
+              >
+                {data.title}
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{ fontFamily: "Raleway-Thin", color: "#e3e3e3" }}
+                gutterBottom
+              >
+                {data.content}
+              </Typography>
+            </Box>
+          ))}
         </Box>
       </Box>
     </>
